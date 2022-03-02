67b75db71982f18aeb29bf49dca0a88630542b14
Mastrakas <71260092+Mastrakas@users.noreply.github.com>
2022-03-02 18:07:38 -0500

    final

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -13,19 +13,53 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class AdminController extends AbstractController{
 
     /**
-     * @Route("choice", name="choice")
+     * @Route("/choice", name="choice")
      */
 
-     public function choice (TechRepository $techRepository){
+     public function choice (TechRepository $techRepository, Request $request){
 
         $tech1 = $techRepository->findAll();
-
         $tech2 = $techRepository->findAll();
 
         return $this->render('choice.html.twig',
         [
             'tech1'=>$tech1,
-            'tech2'=>$tech2
+            'tech2'=>$tech2,
         ]);
      }
+
+     /**
+      * @Route("/myJSON", name="myJSON")
+      */
+
+      public function myJSON (TechRepository $techRepository, Request $request){
+        
+        $tech1 = $_GET["tech1"];
+        $tech2 = $_GET["tech2"];
+        
+        $equivalent1 = $techRepository->findBy(
+            ['name' => $tech1]
+        );
+        $equivalent2 = $techRepository->findBy(
+            ['name' => $tech2]
+        );
+
+        $ninjaName = $equivalent1['0']->getNinjaName()." ".$equivalent2['0']->getNinjaName();
+        
+        $lines = [];
+        $header = [
+            "Ninja Name"
+        ];
+        $lines[] = $header;
+        $lines[] = $ninjaName;
+
+        $filedata = json_encode($lines);
+        $filename = "myJSON.json";
+
+        $response = new Response();
+        $response->headers->set('Cache-Control', 'private');
+        $response->headers->set('Content-Disposition', 'attachment; filename="' .basename($filename). '";');
+        $response->setContent($filedata);
+        return $response;
+      }
 }
